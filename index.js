59d856b31e6ec369b4d7def0736a122edbd65665
Nicholas Michau <nmichau1@asu.edu>
2018-08-25 18:15:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
+function happyHolidayto(par1){
+  return `Happy holidays,${par1}!`
+  
+}
+
 function happyHolidaysto(par1,par2){
   return `Happy ${par1}, ${par2}!`
   
