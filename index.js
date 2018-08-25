f78c5dd851c9e83f533ad704dbcef1f9a831b115
Nicholas Michau <nmichau1@asu.edu>
2018-08-25 18:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "happy holidays!"
+}
+
+function happyHolidayto(par1,par2){
+  return `Happy ${par1}, ${par2}!`
+  
+}
+function holidayCountdown(par1,par2){
+  return `It's ${days} days until ${holiday}!`
+}
